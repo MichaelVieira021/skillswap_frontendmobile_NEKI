1be0cf3e0a13a699e399a9a6e2b67b51b736f37c
MichaelVieira021 <michael21rj@gmail.com>
2024-01-06 14:54:38 -0300

    [CONFIG] Services config
    -Adicionado configurações axios

---
+++ skillswap/src/services/api.ts
@@ -0,0 +1,71 @@
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: 'http://localhost:8080/api'
+})
+
+//CONFIG TOKEN
+export const configurarToken = (token: string) => {
+    api.defaults.headers.common['Authorization'] = token;
+};
+
+export function verificarToken(token: string){
+    return api.post("/usuarios/validar/token", null, {
+        params: {
+            token: token 
+        }
+    });
+};
+
+
+
+//USER
+export function verificarUsuario(login: string, senha: string){
+    return api.post("/usuarios/login", {login, senha})
+};
+
+export function cadastrarNovoUsuario(login: string, senha: string){
+    return api.post("/usuarios", {login, senha})
+};
+
+
+//SKILLS USER
+export function skillsUser(userId: number){
+    return api.get(`/usuarios/skillsUser?userId=${userId}`)
+};
+
+export function levelUp(id: number){
+    return api.patch(`/usuarios/levelUp/skill/${id}`)
+};
+
+export function levelDown(id: number){
+    return api.patch(`/usuarios/levelDown/skill/${id}`)
+};
+
+export function delUserSkill(id: number){
+    return api.delete(`/usuarios/deletar/skill/${id}`)
+};
+
+export function adicionarSkillUser(idUser: number, idSkill: number, level: number){
+    return api.post("/usuarios/adicionar/skill", null, {
+        params: {
+            idUser: idUser,
+            idSkill: idSkill,
+            level: level
+        }
+    });
+};
+
+
+//SKILL
+export function obterTodasSkills(){
+    return api.get("/skills")
+};
+
+export function obterTodasSkillsUserNot(userId: number){
+    return api.get(`/skills/skillsUserNot?userId=${userId}`)
+};
+
+export function obterSkillPorId(id: number){
+    return api.get(`/skills/${id}`)
+};
